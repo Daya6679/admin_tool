538a440402f7efc061c154601d3ad99a9cc054af
daya6679 <danappadayappanavar@gmail.com>
2026-01-13 18:10:57 +0530

    Fix PM2 ecosystem config for Jenkins workspace

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,9 +2,8 @@ module.exports = {
   apps: [
     {
       name: "admin_tool",
-      script: "npm",
-      args: "start",
-      cwd: "/var/www/admin_tool",
+      script: "app.js",
+      cwd: process.cwd(),
       env: {
         NODE_ENV: "production"
       }
