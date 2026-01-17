4d8f659dd6eb88085f26250ec839f7c925564d32
daya6679 <danappadayappanavar@gmail.com>
2026-01-17 16:08:09 +0530

    playwright.config

--- playwright.config.js
+++ playwright.config.js
@@ -67,6 +67,7 @@ export default defineConfig({
   webServer: {
     command: 'npm run start',
     url: 'http://127.0.0.1:3050',
-    reuseExistingServer: !process.env.CI,
+    reuseExistingServer: true,
+    timeout: 120 * 1000,
   },
 });
